3b35470c5343cf943399be740a72d5b5742aeef3
Noah MacMinn <nm4650@cs.ship.edu>
2020-10-28 14:41:26 -0400

    increased testingMap size and player hitbox

--- scripts/TestingMap.gd
+++ scripts/TestingMap.gd
@@ -3,12 +3,14 @@ extends Node2D
 
 onready var player_scene = preload("res://scenes/Player.tscn")
 onready var monster_scene = preload("res://scenes/Monster.tscn")
-
+var SCALE = 1.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var player = player_scene.instance()
 	player.position = Vector2(100, 100)
+	player.scale.x = SCALE
+	player.scale.y = SCALE
 	add_child(player)
 	
 	var monster = monster_scene.instance()
